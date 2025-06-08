d94eca8c4c9b9b8d1e27daf36b80d1e79763646c
ojw0622 <103238089+ojw0622@users.noreply.github.com>
2025-06-09 00:36:11 +0900

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="text-2xl text-center font-bold text-blue-600 mt-10">
+      ✅ Your Bolt Website is Running!
+    </div>
+  )
+}
+
+export default App
